4cf5bab8d7e945a18149d2633fad96f9560337ac
Kuttralanathan-K <kuttral99@gmail.com>
2020-05-01 19:15:54 +0530

    Final edit on starting pages

--- app/src/main/java/julian/com/coronahelpapp/MainActivity.java
+++ app/src/main/java/julian/com/coronahelpapp/MainActivity.java
@@ -4,26 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    Button login;
 
+    EditText name,mobile,address,people;
+    Button submit;
+    String num,per_name,add,locals;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        login=findViewById(R.id.submit);
-        login.setOnClickListener(new View.OnClickListener() {
+        name=findViewById(R.id.name);
+        mobile=findViewById(R.id.mobileno);
+        address=findViewById(R.id.address);
+        people=findViewById(R.id.people);
+        submit=findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,home.class);
-                startActivity(intent);
-                finish();
+            public void onClick(View v) {
+                num=mobile.getText().toString();
+                per_name=name.getText().toString();
+                add=address.getText().toString();
+                locals=people.getText().toString();
+
+                if(TextUtils.isEmpty(per_name)||TextUtils.isEmpty(num)||TextUtils.isEmpty(add)||TextUtils.isEmpty(locals))
+                {
+                    Toast.makeText(MainActivity.this,"Please Enter all the details",Toast.LENGTH_SHORT).show();
+                }else if(num.length()>10||num.length()<10){
+                    Toast.makeText(MainActivity.this,"Please Enter valid number",Toast.LENGTH_SHORT).show();
+                }else{
+                    Intent in = new Intent(MainActivity.this,OTP_Activity.class);
+                    in.putExtra("num",num);
+                    startActivity(in);
+                }
 
             }
         });
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null)
+        {
+            Intent in = new Intent(MainActivity.this,home.class);
+            in.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(in);
+        }
     }
 }
---
+++ app/src/main/java/julian/com/coronahelpapp/OTP_Activity.java
@@ -0,0 +1,114 @@
+package julian.com.coronahelpapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class OTP_Activity extends AppCompatActivity {
+
+    String verifyid;
+    EditText otp;
+    Button login;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_o_t_p_);
+        mAuth = FirebaseAuth.getInstance();
+        otp=findViewById(R.id.otp);
+        login=findViewById(R.id.login);
+        String ph_no="+91"+getIntent().getStringExtra("num");
+        sendverification(ph_no);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = otp.getText().toString();
+                if(TextUtils.isEmpty(code))
+                {
+                    Toast.makeText(OTP_Activity.this,"Enter the code",Toast.LENGTH_LONG).show();
+                }
+                verifycode(code);
+            }
+        });
+    }
+
+    private void verifycode(String code)
+    {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verifyid,code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Intent in = new Intent(OTP_Activity.this,home.class);
+                    in.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(in);
+                    Toast.makeText(OTP_Activity.this,"Login Success",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(OTP_Activity.this,"Signin error",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void sendverification(String num)
+    {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                num,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                TaskExecutors.MAIN_THREAD,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verifyid = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if(code!=null)
+            {
+                verifycode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Toast.makeText(OTP_Activity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+            Log.w("E://////","Faile------->",e);
+        }
+    };
+}
